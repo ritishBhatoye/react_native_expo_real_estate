ff45de3fcb0babcefa7cd86a797907b13c18bcf9
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-02 23:40:31 +0530

    app set-up complete

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View  
+    >
+      <Text className="text-4xl text-red-600">Home</Text>
+    </View>
+  );
+}

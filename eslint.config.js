ddb07c5ccb1363cb66a126bb781bb35c7c7fb6c0
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-05 04:56:20 +0530

    Few amendments

--- eslint.config.js
+++ eslint.config.js
@@ -1,77 +1,36 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import reactNativePlugin from "eslint-plugin-react-native";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
   {
+    ignores: ["node_modules", "build", "ios", "android", ".expo"],
+  },
+  js.configs.recommended,
+  react.configs.recommended,
+  reactHooks.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-      globals: {
-        require: "readonly",
-        module: "readonly",
-        process: "readonly",
-        __dirname: "readonly",
-      },
+      parser: tsParser,
     },
     plugins: {
-      react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
-      "react-native": reactNativePlugin,
+      "@typescript-eslint": ts,
     },
     rules: {
-      // React rules
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-
-      // TypeScript rules
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
-      "@typescript-eslint/no-require-imports": "off", // Allowing require for images
-
-      // React Native rules
-      "react-native/no-unused-styles": "warn",
-      "react-native/split-platform-components": "warn",
-      "react-native/no-inline-styles": "warn",
-      "react-native/no-color-literals": "warn",
-      "react-native/no-raw-text": [
-        "warn",
-        {
-          skip: ["ThemedText"],
-        },
-      ],
-
-      // React Hooks rules
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
-    ignores: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/build/**",
-      "**/.expo/**",
-      "**/web-build/**",
-      "**/babel.config.js",
-      "**/metro.config.js",
-      "**/jest.config.js",
-      "**/tailwind.config.js",
-      "**/app.config.js",
-    ],
   },
-);
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx", ".jsx"] }],
+      "no-unused-vars": "warn",
+    },
+  },
+];

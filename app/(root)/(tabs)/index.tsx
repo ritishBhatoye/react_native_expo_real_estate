e5a505f779eee9b0a685fa48a4836314c4da7580
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-03-08 16:51:54 +0530

    continuing with home

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Image } from 'react-native';
-import icons from '@/constants/icons';
-import images from '@/constants/images';
+
 import UserGreeting from '@/components/home/UserGreeting';
-import { TextInput } from 'react-native-gesture-handler';
-import { useDebouncedCallback } from 'use-debounce';
+
 import SearchField from '@/components/home/SearchField';
 
 export default function Index() {
